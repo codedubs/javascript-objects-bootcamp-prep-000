3c2e944cc1bbe44509eaf7a1e68148b8e7b5fba0
Pitta <mailez4all@gmail.com>
2018-02-04 10:15:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
 
-    playlist["Phil Ochs"] = "Here's to the state of Mississippi"
+    playlist["Phil Ochs"] = "Here's to the State of Mississippi"
     return playlist
 
 }
